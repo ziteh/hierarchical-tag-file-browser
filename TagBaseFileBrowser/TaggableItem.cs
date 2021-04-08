0a10a6dd63148c5a2cdf85f899d8446be1d95096
ziteh <honmonoh@gmail.com>
2021-04-08 19:58:50 +0800

    Add TaggableItem.cs

---
+++ TagBaseFileBrowser/TaggableItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagBaseFileBrowser
+{
+    internal abstract class TaggableItem
+    {
+       public int Id { get; set; }
+       public string[] Names { get; set; }
+       public string Description { get; set; }
+       public string Path { get; set; }
+       public string ThumbnailPath { get; set; }
+       public string Remark { get; set; }
+       public Tag[] Tags { get; set; }
+    }
+
+    class Tag : TaggableItem
+    {
+        
+    }
+
+    class File : TaggableItem
+    {
+        
+    }
+
+    class Folder : TaggableItem
+    {
+        
+    }
+}
