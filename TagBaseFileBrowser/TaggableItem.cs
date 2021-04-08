dddfca6c818ca7fe2fd852035db650f57e40cdb4
ziteh <honmonoh@gmail.com>
2021-04-08 20:01:58 +0800

    將陣列改成List<>

--- TagBaseFileBrowser/TaggableItem.cs
+++ TagBaseFileBrowser/TaggableItem.cs
@@ -9,12 +9,12 @@ namespace TagBaseFileBrowser
     internal abstract class TaggableItem
     {
        public int Id { get; set; }
-       public string[] Names { get; set; }
+       public List<string> Names { get; set; }
        public string Description { get; set; }
        public string Path { get; set; }
        public string ThumbnailPath { get; set; }
        public string Remark { get; set; }
-       public Tag[] Tags { get; set; }
+       public List<Tag> Tags { get; set; }
     }
 
     class Tag : TaggableItem
