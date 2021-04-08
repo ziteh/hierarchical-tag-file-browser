5422f90478e1d8e04cdb3d074595728dde1f2740
ziteh <honmonoh@gmail.com>
2021-04-08 20:27:22 +0800

    實作ITaggableTiemIO

--- TagBaseFileBrowser/TaggableItemIO.cs
+++ TagBaseFileBrowser/TaggableItemIO.cs
@@ -10,8 +10,21 @@ namespace TagBaseFileBrowser
         void Write(string file, List<TaggableItem> taggableItems);
     }
     
-    public class TaggableItemIO
+    public class TaggableItemIO : ITaggableItemIO
     {
-        
+        public List<TaggableItem> Read(string file)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Write(string file, TaggableItem taggableItem)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Write(string file, List<TaggableItem> taggableItems)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
