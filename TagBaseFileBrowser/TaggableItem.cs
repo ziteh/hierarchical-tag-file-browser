baa4969a945ecd5b58821582077d6bd7d8627cea
ziteh <honmonoh@gmail.com>
2021-04-08 20:17:19 +0800

    增加各欄位的屬性

--- TagBaseFileBrowser/TaggableItem.cs
+++ TagBaseFileBrowser/TaggableItem.cs
@@ -16,6 +16,48 @@ namespace TagBaseFileBrowser
         protected string _remark;
         protected List<Tag> _tags;
 
+        public virtual int Id
+        {
+            get => _id;
+            set => _id = value;
+        }
+
+        public virtual List<string> Names
+        {
+            get => _names;
+            set => _names = value;
+        }
+
+        public virtual string Description
+        {
+            get => _description;
+            set => _description = value;
+        }
+
+        public virtual string Path
+        {
+            get => _path;
+            set => _path = value;
+        }
+
+        public virtual string ThumbnailPath
+        {
+            get => _thumbnailPath;
+            set => _thumbnailPath = value;
+        }
+
+        public virtual string Remark
+        {
+            get => _remark;
+            set => _remark = value;
+        }
+
+        public virtual List<Tag> Tags
+        {
+            get => _tags;
+            set => _tags = value;
+        }
+
         public void AddTag(Tag tag)
         {
             _tags.Add(tag);
@@ -33,12 +75,8 @@ namespace TagBaseFileBrowser
     }
 
     class File : TaggableItem
-    {
-        
-    }
+    { }
 
     class Folder : TaggableItem
-    {
-        
-    }
+    { }
 }
