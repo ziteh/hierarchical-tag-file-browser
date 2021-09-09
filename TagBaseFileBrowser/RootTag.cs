e9036de42bd88699fd6ec50e2f030a94d0e78002
ziteh <honmonoh@gmail.com>
2021-09-09 18:28:08 +0800

    Add RootTag

---
+++ TagBaseFileBrowser/RootTag.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagBaseFileBrowser
+{
+    internal class RootTag : Taggable
+    {
+        public RootTag()
+        {
+            Name = "root";
+            Id = "0";
+        }
+    }
+}
