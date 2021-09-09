ac034e88f5feb4757de6f1362ef03aece4c581c2
ziteh <honmonoh@gmail.com>
2021-09-09 16:48:40 +0800

    Add XmlTagDatabaseIO

---
+++ TagBaseFileBrowser/IO/XmlTagDatabaseIO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagBaseFileBrowser.IO
+{
+    public class XmlTagDatabaseIO : ITagDatabaseIO
+    {
+        public List<Tag> Read(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Write(string path, Tag tag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Write(string path, List<Tag> tags)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
