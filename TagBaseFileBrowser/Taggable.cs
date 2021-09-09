dbefed52991f0de46914f4d92c8985457fa8c4de
ziteh <honmonoh@gmail.com>
2021-09-09 18:12:31 +0800

    Add IdParse()

--- TagBaseFileBrowser/Taggable.cs
+++ TagBaseFileBrowser/Taggable.cs
@@ -14,34 +14,39 @@ namespace TagBaseFileBrowser
 
     public class Tag : Taggable
     {
-        public Tag(string name, TagType type = TagType.General)
+        public Tag(string name, TagType type = TagType.General, List<Tag> parentTags = null)
         {
             Name = name;
             Type = type;
-        }
+            ParentTags = parentTags ?? new List<Tag>();
 
-        public Tag(string[] name, TagType type = TagType.General)
-        {
-            Name = name[0];
-            Type = type;
-            if (name.Length > 1)
-            {
-                for (var i = 1; i < name.Length; i++)
-                {
-                    Alias.Add(name[i]);
-                }
-            }
+            Id = ParseId();
         }
 
         public Color BackgroundColor { get; set; } = Color.White;
         public Color FontColor { get; set; } = Color.Black;
         public TagType Type { get; set; }
+
+        private string ParseId()
+        {
+            var parentTag = "";
+            if (this.ParentTags != null && this.ParentTags.Count > 0)
+            {
+                for (var i = 0; i < this.ParentTags.Count; i++)
+                {
+                    parentTag += ParentTags[i].Name;
+                }
+            }
+
+            var keyString = $"{this.Name}{this.Type}{parentTag}";
+            return keyString.ToMD5();
+        }
     }
 
     public abstract class Taggable
     {
         public List<string> Alias { get; set; }
-        public int Id { get; }
+        public string Id { get; protected set; }
         public string Name { get; set; }
         public List<Tag> ParentTags { get; set; }
         public string Remark { get; set; }
