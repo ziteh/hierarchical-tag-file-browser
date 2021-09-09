7bebd2ae2d66f84620048ae3edc79b0452a9012b
ziteh <honmonoh@gmail.com>
2021-09-09 16:38:10 +0800

    將不同的TagsIO分開到不同的檔案

---
+++ TagBaseFileBrowser/IO/CsvTagsIO.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagBaseFileBrowser.IO
+{
+    public class CsvTagsIo : ITagsIO
+    {
+        public List<Tag> Read(string file)
+        {
+            //var tags = new List<Tag>();
+            //var csvContent = Csv.Read(file);
+
+            //foreach (var rowOfCsv in csvContent)
+            //{
+            //    var tag = new Tag(rowOfCsv[1].Split(';'))
+            //    {
+            //        ThumbnailPath = rowOfCsv[7],
+            //        Remark = rowOfCsv[8]
+            //    };
+            //    tag.AddTag(rowOfCsv[3].Split(';'));
+
+            //    tags.Add(tag);
+            //}
+
+            //return tags;
+        }
+
+        public void Write(string path, Tag tag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Write(string path, List<Tag> tags)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TagBaseFileBrowser/IO/FackTagsIO.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagBaseFileBrowser.IO
+{
+    public class FakeTagsIO : ITagsIO
+    {
+        public List<Tag> Read(string file)
+        {
+            return new List<Tag>
+            {
+                new Tag(new []{"fake name-1"})
+                {
+                    ThumbnailPath = "fake thumbnail path",
+                    Remark = "fake remark",
+                    ParentTags = new List<Tag>
+                    {
+                        new Tag("fake tag-1"),
+                        new Tag("fake tag-2"),
+                    }
+                },
+                new Tag(new []{"fake name-2"})
+                {
+                    ThumbnailPath = "fake thumbnail path",
+                    Remark = "fake remark",
+                    ParentTags = new List<Tag>
+                    {
+                        new Tag("fake tag-1"),
+                        new Tag("fake tag-2"),
+                    }
+                }
+            };
+        }
+
+        public void Write(string path, Tag tag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Write(string path, List<Tag> tags)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
