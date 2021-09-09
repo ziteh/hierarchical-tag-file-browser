bd25d7f6e45203ad0b11edd7f6b70cb693f3cf16
ziteh <honmonoh@gmail.com>
2021-09-09 17:00:22 +0800

    Add XmlNode

---
+++ TagBaseFileBrowser/IO/XmlNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagBaseFileBrowser.IO
+{
+    public static class XmlNode
+    {
+        public static string Alias = "Alias";
+        public static string BackgroundColor = "backgroundColor";
+        public static string FontColor = "fontColor";
+        public static string Name = "name";
+        public static string ParentTags = "parentTags";
+        public static string remark = "remark";
+        public static string ThumbnailPath = "thumbnailPath";
+        public static string Type = "type";
+    }
+}
