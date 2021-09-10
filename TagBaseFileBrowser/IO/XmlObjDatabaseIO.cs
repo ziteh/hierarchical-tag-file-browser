69002a8a86ddecde5dcb2be7f33aa75a9c540a94
ziteh <honmonoh@gmail.com>
2021-09-10 10:23:36 +0800

    Add XmlObjDatabaseIO class

---
+++ TagBaseFileBrowser/IO/XmlObjDatabaseIO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagBaseFileBrowser.IO
+{
+    public class XmlObjDatabaseIO : IObjDatabaseIO
+    {
+        public List<Obj> Read(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
