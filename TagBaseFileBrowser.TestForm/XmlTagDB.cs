3553891b8a2f7fd45a4aaec528d964968904866c
ziteh <honmonoh@gmail.com>
2021-09-10 09:35:11 +0800

    Add TagBaseFileBroeser.TestForm

---
+++ TagBaseFileBrowser.TestForm/XmlTagDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TagBaseFileBrowser.TestForm
+{
+    public partial class XmlTagDB : Form
+    {
+        public XmlTagDB()
+        {
+            InitializeComponent();
+        }
+    }
+}
