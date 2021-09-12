e0ae09af5bacca08f126e6ad4324946dabf5b96f
ziteh <honmonoh@gmail.com>
2021-09-12 10:22:48 +0800

    Add UI

--- TagBaseFileBrowser/MainForm.Designer.cs
+++ TagBaseFileBrowser/MainForm.Designer.cs
@@ -29,13 +29,95 @@ namespace TagBaseFileBrowser
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.treeViewTags = new System.Windows.Forms.TreeView();
+            this.listViewObjs = new System.Windows.Forms.ListView();
+            this.textBoxObjInfo = new System.Windows.Forms.TextBox();
+            this.buttonAddTag = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Location = new System.Drawing.Point(13, 13);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(253, 25);
+            this.textBoxPath.TabIndex = 0;
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(273, 14);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(75, 23);
+            this.buttonLoad.TabIndex = 1;
+            this.buttonLoad.Text = "Load";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // treeViewTags
+            // 
+            this.treeViewTags.Location = new System.Drawing.Point(13, 45);
+            this.treeViewTags.Name = "treeViewTags";
+            this.treeViewTags.Size = new System.Drawing.Size(222, 436);
+            this.treeViewTags.TabIndex = 2;
+            this.treeViewTags.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeViewTags_AfterSelect);
+            // 
+            // listViewObjs
+            // 
+            this.listViewObjs.HideSelection = false;
+            this.listViewObjs.Location = new System.Drawing.Point(241, 45);
+            this.listViewObjs.Name = "listViewObjs";
+            this.listViewObjs.Size = new System.Drawing.Size(508, 346);
+            this.listViewObjs.TabIndex = 3;
+            this.listViewObjs.UseCompatibleStateImageBehavior = false;
+            this.listViewObjs.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.listViewObjs_ItemSelectionChanged);
+            this.listViewObjs.DoubleClick += new System.EventHandler(this.listViewObjs_DoubleClick);
+            // 
+            // textBoxObjInfo
+            // 
+            this.textBoxObjInfo.Location = new System.Drawing.Point(241, 397);
+            this.textBoxObjInfo.Multiline = true;
+            this.textBoxObjInfo.Name = "textBoxObjInfo";
+            this.textBoxObjInfo.ReadOnly = true;
+            this.textBoxObjInfo.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxObjInfo.Size = new System.Drawing.Size(508, 84);
+            this.textBoxObjInfo.TabIndex = 4;
+            // 
+            // buttonAddTag
+            // 
+            this.buttonAddTag.Location = new System.Drawing.Point(791, 45);
+            this.buttonAddTag.Name = "buttonAddTag";
+            this.buttonAddTag.Size = new System.Drawing.Size(75, 23);
+            this.buttonAddTag.TabIndex = 5;
+            this.buttonAddTag.Text = "Add Tag";
+            this.buttonAddTag.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(988, 508);
+            this.Controls.Add(this.buttonAddTag);
+            this.Controls.Add(this.textBoxObjInfo);
+            this.Controls.Add(this.listViewObjs);
+            this.Controls.Add(this.treeViewTags);
+            this.Controls.Add(this.buttonLoad);
+            this.Controls.Add(this.textBoxPath);
+            this.Name = "MainForm";
+            this.Text = "Hierarchical Tag File Browser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.TreeView treeViewTags;
+        private System.Windows.Forms.ListView listViewObjs;
+        private System.Windows.Forms.TextBox textBoxObjInfo;
+        private System.Windows.Forms.Button buttonAddTag;
     }
 }
 
--- TagBaseFileBrowser/MainForm.cs
+++ TagBaseFileBrowser/MainForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,53 @@ namespace TagBaseFileBrowser
 {
     public partial class MainForm : Form
     {
+        private TaggableItemHandler _taggableItemHandler;
+
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            var loadPath = textBoxPath.Text ?? "";
+            _taggableItemHandler = new TaggableItemHandler(loadPath);
+            _taggableItemHandler.CreatTagTreeView(treeViewTags);
+            _taggableItemHandler.CreateObjsListView(listViewObjs);
+        }
+
+        private void listViewObjs_DoubleClick(object sender, EventArgs e)
+        {
+            try
+            {
+                var path = listViewObjs.SelectedItems[0].SubItems[2].Text;
+                Process.Start(_taggableItemHandler.ReplaceParameter(path));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void listViewObjs_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            try
+            {
+                var selectedObjName = listViewObjs.SelectedItems[0].Text;
+                var objInfo = _taggableItemHandler.GetInfo(_taggableItemHandler.FindObjByName(selectedObjName));
+                textBoxObjInfo.Text = objInfo;
+            }
+            catch { }
+        }
+
+        private void treeViewTags_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            var selected = treeViewTags.SelectedNode;
+            if (selected != null)
+            {
+                var selectedTag = _taggableItemHandler.FindTagByName(selected.Text);
+                _taggableItemHandler.UpdateObjListView(listViewObjs, selectedTag);
+            }
         }
     }
 }
