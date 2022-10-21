b8158b3f618e6865c40f5e406303de23f16a82f4
ziteh <honmonoh@gmail.com>
2022-10-21 23:19:55 +0800

    Add 'AddFileForm'

---
+++ vs_sql/TagBaseFileBrowser/TagBaseFileBrowser/AddFileForm.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace TagBaseFileBrowser
+{
+    partial class AddFileForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxParentTag = new System.Windows.Forms.ComboBox();
+            this.textBoxFileName = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // comboBoxParentTag
+            // 
+            this.comboBoxParentTag.FormattingEnabled = true;
+            this.comboBoxParentTag.Location = new System.Drawing.Point(308, 84);
+            this.comboBoxParentTag.Name = "comboBoxParentTag";
+            this.comboBoxParentTag.Size = new System.Drawing.Size(121, 20);
+            this.comboBoxParentTag.TabIndex = 0;
+            // 
+            // textBoxFileName
+            // 
+            this.textBoxFileName.Location = new System.Drawing.Point(518, 82);
+            this.textBoxFileName.Name = "textBoxFileName";
+            this.textBoxFileName.Size = new System.Drawing.Size(100, 22);
+            this.textBoxFileName.TabIndex = 1;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(366, 167);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 2;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.AccessibleDescription = "";
+            this.textBoxPath.AccessibleName = "";
+            this.textBoxPath.Location = new System.Drawing.Point(518, 110);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(100, 22);
+            this.textBoxPath.TabIndex = 1;
+            // 
+            // AddFileForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.textBoxFileName);
+            this.Controls.Add(this.comboBoxParentTag);
+            this.Name = "AddFileForm";
+            this.Text = "AddFile";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxParentTag;
+        private System.Windows.Forms.TextBox textBoxFileName;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.TextBox textBoxPath;
+    }
+}
